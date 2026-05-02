f1b1f70117ceb8a4067eada2f47a39ffa7dbffea
Himanshu Anand <himanshuanand563@gmail.com>
2026-05-02 14:41:55 +0530

    change new syantax style

--- chai_code_day_12/src/main.jsx
+++ chai_code_day_12/src/main.jsx
@@ -2,32 +2,54 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import Layout from './Layout';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '',
-        element: <Home/>,
-      },
-      {
-        path: 'about',
-        element: <About/>,
-      },
-      {
-        path: 'contact',
-        element: <Contact/>,
-      },
-    ],
-  },
-]);
+// this is only style of syntex change 
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home/>,
+//       },
+//       {
+//         path: 'about',
+//         element: <About/>,
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact/>,
+//       },
+//     ],
+//   },
+// ]);
+
+// secon type of syntex 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+  
+    </Route>,
+  ),
+);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
