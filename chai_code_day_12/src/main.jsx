7b034e36098b433ca787783f5bfd864ef899d576
Himanshu Anand <himanshuanand563@gmail.com>
2026-05-02 14:49:52 +0530

    user is used

--- chai_code_day_12/src/main.jsx
+++ chai_code_day_12/src/main.jsx
@@ -12,6 +12,7 @@ import Layout from './Layout';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import User from './components/User/User';
 
 // this is only style of syntex change 
 
@@ -44,7 +45,7 @@ const router = createBrowserRouter(
       <Route path="" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
-  
+      <Route path="user/:userid" element={<User />} />
     </Route>,
   ),
 );
